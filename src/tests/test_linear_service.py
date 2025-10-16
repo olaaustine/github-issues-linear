dd67dcf8433143cba6fd92b79ba16ced0cf1874c
Ola Austine <olaaustine@Olas-MacBook-Pro.local>
2025-10-16 01:10:33 +0100

    feat: adding some tests

--- src/tests/test_linear_service.py
+++ src/tests/test_linear_service.py
@@ -5,7 +5,7 @@ from src.linear_service import LinearService
 
 # Test for get_data_and_populate_variables
 @patch("src.linear_service.requests.post")
-def test_get_data_and_populate_variables(mock_post):
+def test_get_data_and_populate_variables_raises_exception(mock_post):
     # Mock the response for team lookup
     mock_response = MagicMock()
     mock_response.status_code = 200
@@ -24,6 +24,34 @@ def test_get_data_and_populate_variables(mock_post):
     issue2.body = "b2"
     with pytest.raises(RuntimeError):
         linear_service.get_data_and_populate_variables([issue1, issue2])
+
+
+@patch("src.linear_service.requests.post")
+def test_get_data_and_populate_variables_success(mock_post):
+    # Mock the response for team lookup
+    valid_uuid = "123e4567-e89b-12d3-a456-426614174000"
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.text = '{"data":{}}'
+    mock_response.json.return_value = {
+        "data": {"teams": {"nodes": [{"id": "mocked_team_id", "name": "tid"}]}}
+    }
+    mock_post.return_value = mock_response
+    linear_service = LinearService()
+    linear_service._LinearService__team_id = valid_uuid  # Mock team_id directly
+    issue1 = MagicMock()
+    issue1.title = "t1"
+    issue1.body = "b1"
+    issue2 = MagicMock()
+    issue2.title = "t2"
+    issue2.body = "b2"
+    var_list = linear_service.get_data_and_populate_variables([issue1, issue2])
+    assert len(var_list) == 2
+    assert var_list[0].title == "t1"
+    assert var_list[0].as_input() == {
+        "input": {"title": "t1", "description": "b1", "teamId": valid_uuid}
+    }
+    assert var_list[1].title == "t2"
 
 
 # Test for run_query_creates_new
